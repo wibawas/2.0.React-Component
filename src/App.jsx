0f6fa84b3084982a67512e36c777e24529ff025e
wibawas <119534701+wibawas@users.noreply.github.com>
2023-11-03 13:07:09 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./heading";
+import Menu from "./menu";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <ul>
+      <li>Kebab</li>
+      <li>Pizza</li>
+      <li>Susi</li>
+    </ul>
+  );
+}
+
+export default Menu;
